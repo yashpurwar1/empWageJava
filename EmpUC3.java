bfc99cac9ec1072a19a53bb1f4edaecbe609cdf4
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-01 19:07:02 +0530

    Added part time wage

---
+++ EmpUC3.java
@@ -0,0 +1,25 @@
+package empWagePrblm;
+
+public class EmpUC3 {
+	static int wagePerHour=20;
+	static int partTimeHour=4;
+	static int fullDayHour=8;
+	public static void main(String[] args) {
+		int dailyWage, partTimeWage;
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		System.out.println(empCheck);
+		if (empCheck==0) {
+			System.out.println("Employee is absent");
+		}
+		else if (empCheck==1){
+			System.out.println("Employee is present");
+			dailyWage=wagePerHour*fullDayHour;
+			System.out.println("DAily wage= "+dailyWage);
+		}
+		else {
+			System.out.println("Employee parttime present");
+			partTimeWage=partTimeHour*wagePerHour;
+			System.out.println("Parttime wage = "+partTimeWage);
+		}
+	}
+}
