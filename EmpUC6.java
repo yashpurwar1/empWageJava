2dbdd83b477bec1f40eb04f9b78904e49e2c0902
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-02 00:09:23 +0530

    Added condition for 100 hours

---
+++ EmpUC6.java
@@ -0,0 +1,41 @@
+package empWagePrblm;
+
+public class EmpUC6 {
+	static int wagePerHour = 20;
+	static int partTimeHour = 4;
+	static int fullDayHour = 8;
+
+	public static void main(String[] args) {
+		int dayWage = 0;
+		int days, hour=0;
+		int totalWage=0;
+		for (days=1; days<=20; days++) {
+			if (hour>=100) {
+				break;
+			}
+			int empCheck=(int) Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+			case 0:
+				System.out.println("Employee is absent");
+				dayWage=0;
+				hour=hour+0;
+				break;
+			case 1:
+				System.out.println("Employee is present");
+				dayWage=wagePerHour*fullDayHour;
+				System.out.println("DAily wage= "+dayWage);
+				hour=hour+8;
+				break;
+			case 2:
+				System.out.println("Employee parttime present");
+				dayWage=partTimeHour*wagePerHour;
+				System.out.println("Parttime wage = "+dayWage);
+				hour=hour+4;
+				break;
+			}
+			System.out.println(hour);
+			totalWage=totalWage+dayWage;
+		}
+		System.out.println("Wage for the month is "+ totalWage);
+	}
+}
