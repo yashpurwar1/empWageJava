36d2927a0a3b6e2cad72b65e7dfb650595be8401
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-01 23:41:17 +0530

    Done using Switch

---
+++ EmpUC4.java
@@ -0,0 +1,27 @@
+package empWagePrblm;
+
+public class EmpUC4 {
+	static int wagePerHour=20;
+	static int partTimeHour=4;
+	static int fullDayHour=8;
+	public static void main(String[] args) {
+		int dailyWage, partTimeWage;
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		System.out.println(empCheck);
+		switch (empCheck) {
+		case 0:
+			System.out.println("Employee is absent");
+			break;
+		case 1:
+			System.out.println("Employee is present");
+			dailyWage=wagePerHour*fullDayHour;
+			System.out.println("DAily wage= "+dailyWage);
+			break;
+		case 2:
+			System.out.println("Employee parttime present");
+			partTimeWage=partTimeHour*wagePerHour;
+			System.out.println("Parttime wage = "+partTimeWage);
+			break;
+		}
+	}
+}
